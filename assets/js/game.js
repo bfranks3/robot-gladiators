477659c5692174e00cf52b3f67b1995de4ffcbe6
Brandon <captainxcuddles@hotmail.com>
2022-02-22 19:12:59 -0700

    adding some JS to the file

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,6 @@
 var playerName = window.prompt("What is your robot's name?");
+// Note the lack of quotations marks around playerName
+console.log(playerName);
 console.log("This logs a string, good for leaving yourself a message");
 // this will do math and log 20
 console.log(10 + 10);
